16df26e0a4306ab92a33846ad4f73131a0da2388
Sanchosrancho <sanchosrancho@gmail.com>
2014-05-02 12:22:19 +0400

    BabyDateBirth Controller was added

---
+++ ParentsAcademy/BabyDateBirthViewController.h
@@ -0,0 +1,15 @@
+//
+//  BabyDateBirthViewController.h
+//  ParentsAcademy
+//
+//  Created by Alex on 5/2/14.
+//  Copyright (c) 2014 Alex Shevlyakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BabyDateBirthViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIDatePicker *birthDatePicker;
+
+@end
